36d77466508b822caacab200b0f2c5a8e1346e27
Dorde Kesic <dordekesic.keso@gmail.com>
2023-11-01 10:20:32 +0100

    adds typesafety to route function

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,12 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
+import { AuthEndpoints, handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest, { params }: any) {
+type Params = { kindeAuth: AuthEndpoints };
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Params }
+) {
     const endpoint = params.kindeAuth;
     return handleAuth(request, endpoint);
 }
